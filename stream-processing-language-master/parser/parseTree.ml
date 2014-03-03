d2fe958aca0f97376af6c2e8021eeea8c8e234a7
Tung Nguyen <tn2g11@ecs.soton.ac.uk>
2014-03-03 18:56:27 +0000

    Rearrange stuf

--- stream-processing-language-master/parser/parseTree.ml
+++
@@ -1,8 +0,0 @@
-
-type pTree = 
-  | Leaf of (int * int)
-  | Name of (string * int)
-  | Node1 of (string * pTree * int)
-  | Node2 of (string * pTree * pTree * int) 
-  | Node3 of (string * pTree * pTree * pTree * int) ;;
-
