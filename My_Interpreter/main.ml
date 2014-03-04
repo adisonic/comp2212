a8654e74e54e7b94de0bb42cd9c5c158e6221db9
Adi <acc22g12@soton.ac.uk>
2014-03-04 21:21:21 +0000

    Update main.ml

--- My_Interpreter/main.ml
+++ My_Interpreter/main.ml
@@ -4,11 +4,23 @@ open mylexer;
 open myparser;
 open myparseTree;
 
+module MapOfVariables = map.make(String);;
+let GlobalVariables = ref MapOfVariables.empty;;
+
+(*The below variables are to store information on the stream*)
+let streamCount = ref 0;
+let streamLength = ref 0;
+
+
 let rec recursivePath inputtree =
   
   let processBody body = 
     (recursivePath body)
   in
+  
+  let processBody extend bodyOne bodyTwo = 
+    (recursivePath bodyOne);
+    (recursivePath bodyTwo);
     
   let processIf condition body = 
     if ((recursivePath condition) == true)
@@ -30,10 +42,10 @@ let rec recursivePath inputtree =
     
 
 match inputtree with
-    LeafBool(argBool)                    -> argBool
-  | Leaf (arg1)                       -> arg1
-  | Variable (name)                 -> (AssignName name)
-  | Node1("streamValue", streamName)            ->
+    LeafBool(argBool)                   -> argBool
+  | Leaf (arg1)                         -> arg1
+  | Variable (name)                     -> (AssignName name)
+  | Node1("streamValue", streamName)    -> (processStream streamName) 
   
     Node1("++", arg1)
   | Node1("--", arg1)
@@ -45,13 +57,13 @@ match inputtree with
   | Node2("%", arg1, arg2)              -> (recursivePath arg1) mod (recursivePath arg2)
   | Node2("^", arg1, arg2)              -> (recursivePath arg1) ** (recursivePath arg2)
   
-  | Node1("bodyEnding", arg1)         -> (processBody arg1
-  | Node2("bodyExtend", arg1, arg2)
+  | Node1("bodyEnding", arg1)         -> (recursivePath arg1)
+  | Node2("bodyExtend", arg1, arg2)   -> (recursivePath arg1); (recursivePath arg2)
   
   | Node2("globalAssign", arg1, arg2)
   | Node2("globalAssignExtending", arg1, arg2)
   
-  | Node2("MainwithGlobalVars", arg1, arg2)
+  | Node2("MainwithGlobalVars", arg1, arg2)   -> (recursivePath arg1); (recursivePath arg2)
   
   | Node2("if", arg1, arg2)             -> (processIf arg1 arg2)
   | Node3("if", arg1, arg2, arg3)         -> (processIfElse arg1 arg2 arg3)
@@ -65,5 +77,5 @@ match inputtree with
   | Node2(">=", arg1, arg2)             -> if ((recursivePath arg1) >= (recursivePath arg2)) then true else false
   | Node2("<", arg1, arg2)              -> if ((recursivePath arg1) < (recursivePath arg2)) then true else false
   | Node2("<=", arg1, arg2)             -> if ((recursivePath arg1) <= (recursivePath arg2)) then true else false
-  | Node1("!", arg1)                  -> if ((recursivePath arg1) == true) then false else true
+  |(* Node1("!", arg1)                  -> if ((recursivePath arg1) == true) then false else true  *)
   | 
