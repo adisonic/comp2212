4399daacca5eea04f5a8d64c9c55f3432bc767f1
Adi <acc22g12@soton.ac.uk>
2014-03-04 12:29:30 +0000

    Update myparseTree.ml

--- My_Interpreter/myparseTree.ml
+++ My_Interpreter/myparseTree.ml
@@ -3,7 +3,7 @@
 type parseTree = 
 	| Leaf of (int) 
 	| LeafBool of (bool)
-	| Name of (string)
+	| Variable of (string)
 	| Node1 of (string * parseTree)
 	| Node2 of (string * parseTree * parseTree)
 	| Node3 of (string * parseTree * parseTree * parseTree)
