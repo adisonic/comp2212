1587bd07b20658d07174495ed4088468b63746af
tungcs <hatungnguyen91@gmail.com>
2014-03-07 21:17:24 +0000

    Update Handle_Nodes_Checklist.ml

--- My_Interpreter/Handle_Nodes_Checklist.ml
+++ My_Interpreter/Handle_Nodes_Checklist.ml
@@ -1,46 +1,46 @@
 main:
   BEGIN LCURLYB body RCURLYB { $3 }
-  | globalVars BEGIN LCURLYB body RCURLYB { Node2("MainWithGlobalVars", $1, $4)}
+  | globalVars BEGIN LCURLYB body RCURLYB { Node2("MainWithGlobalVars", $1, $4)} (* done *)
 ;
 
 globalVars:
- | STRING ASSIGN rawvalue SEMI_COLON { Node2("globalAssign", Variable($1), $3)}
- | STRING ASSIGN rawvalue SEMI_COLON globalVars { Node2("globalAssignExtending", Node2("globalAssign", Variable($1), $3), $5)} /*Check main to see if handled properly*/
+ | STRING ASSIGN rawvalue SEMI_COLON { Node2("globalAssign", Variable($1), $3)} (* done *)
+ | STRING ASSIGN rawvalue SEMI_COLON globalVars { Node2("globalAssignExtending", Node2("globalAssign", $1, $3), $5)} /*Check main to see if handled properly*/
 ;
   
 body:
-     sentence { Node1("bodyEnding", $1) }
-     | sentence body { Node2("bodyExtend", $1, $2) }
+     sentence { Node1("bodyEnding", $1) } (* done *)
+     | sentence body { Node2("bodyExtend", $1, $2) } (* done *)
 ;
 
 /* binding: variable and its value */
 
 variable:
-   | STRING { Variable($1)}
-   | OPENSTREAM rawvalue CLOSESTREAM { Node1("streamValue", $2) }
+   | STRING { Variable($1)} (*done *)
+   | OPENSTREAM rawvalue CLOSESTREAM { Node1("streamValue", $2) } (* done *)
 
 /* if ( value ) { body }
    if ( value ) { body } else { body }
    if ( value ) { body } else_if */
    
 cond_statement:
-   IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2("if", $3, $6) }
-   | IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB ELSE LCURLYB body RCURLYB { Node3("if", $3, $6, $10) }
+   IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2("if", $3, $6) } (* done *)
+   | IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB ELSE LCURLYB body RCURLYB { Node3("if", $3, $6, $10) } (* done *)
    /* May need to add elseif */
 
                     
 
 /* while ( value ) { body } */
 while_statement: /* loop */
-   | WHILE LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2 ("while", $3, $6) }
+   | WHILE LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2 ("while", $3, $6) } (* done *)
 ;
    
    
 sentence: /* is a statement form a complete instruction, could include a semicolon (?) */
    | rawvalue SEMI_COLON { $1 }
-   | variable ASSIGN rawvalue SEMI_COLON { Node2("assign", $1, $3) }
-   | variable INCRE_EQUAL rawvalue SEMI_COLON { Node2("+=", $1, $3) }
-   | variable DECRE_EQUAL rawvalue SEMI_COLON { Node2("-=", $1, $3) } /* may need to add *= and /= */
+   | variable ASSIGN rawvalue SEMI_COLON { Node2("assign", $1, $3) } (* Not included yet *)
+   | variable INCRE_EQUAL rawvalue SEMI_COLON { Node2("+=", $1, $3) } (* Not included yet *)
+   | variable DECRE_EQUAL rawvalue SEMI_COLON { Node2("-=", $1, $3) } (* Not included yet *) /* may need to add *= and /= */
    | cond_statement { $1 }
    | while_statement { $1 }
 ;
@@ -53,26 +53,26 @@ rawvalue:
    INT { Leaf($1) }
  | LPAREN rawvalue RPAREN { $2 }
  
- | variable INCRE /* ++ */ { Node1("++", $1) }
- | variable DECRE /* -- */ { Node1("--", $1) }
+ | variable INCRE /* ++ */ { Node1("++", $1) } (* done *)
+ | variable DECRE /* -- */ { Node1("--", $1) } (* done *)
  
- | rawvalue PLUS rawvalue { Node2("+", $1, $3) }
- | rawvalue MINUS rawvalue { Node2("-", $1, $3) }
- | rawvalue TIMES rawvalue { Node2("*", $1, $3) }
- | rawvalue DIV rawvalue { Node2("/", $1, $3) }
- | rawvalue MOD rawvalue { Node2("%", $1, $3) }
- | rawvalue EXP rawvalue { Node2("^", $1, $3) }
+ | rawvalue PLUS rawvalue { Node2("+", $1, $3) } (* done *)
+ | rawvalue MINUS rawvalue { Node2("-", $1, $3) } (* done *)
+ | rawvalue TIMES rawvalue { Node2("*", $1, $3) } (* done *)
+ | rawvalue DIV rawvalue { Node2("/", $1, $3) } (* done *)
+ | rawvalue MOD rawvalue { Node2("%", $1, $3) } (* done *)
+ | rawvalue EXP rawvalue { Node2("^", $1, $3) } (* done *)
  /*| MINUS rawvalue %prec UMINUS { - $2 } */
   
- | rawvalue AND rawvalue { Node2("&&", $1, $3) }
- | rawvalue OR rawvalue { Node2("||", $1, $3) }
- | NOT rawvalue { Node1("!", $2 )}
- | rawvalue EQUAL rawvalue { Node2("=", $1, $3) }
- | rawvalue NOT_EQUAL rawvalue { Node2("!=", $1, $3) }
- | rawvalue GREATER rawvalue { Node2(">", $1, $3) }
- | rawvalue GREATER_OR_EQUAL rawvalue { Node2(">=", $1, $3) }
- | rawvalue LESSER rawvalue { Node2("<", $1, $3) }
- | rawvalue LESSER_OR_EQUAL rawvalue { Node2("<=", $1, $3) }
+ | rawvalue AND rawvalue { Node2("&&", $1, $3) } (* done *)
+ | rawvalue OR rawvalue { Node2("||", $1, $3) } (* done *)
+ | NOT rawvalue { Node1("!", $2 )} (* done *) (* done *)
+ | rawvalue EQUAL rawvalue { Node2("==", $1, $3) } 
+ | rawvalue NOT_EQUAL rawvalue { Node2("!=", $1, $3) } (* done *)
+ | rawvalue GREATER rawvalue { Node2(">", $1, $3) } (* done *) 
+ | rawvalue GREATER_OR_EQUAL rawvalue { Node2(">=", $1, $3) } (* done *)
+ | rawvalue LESSER rawvalue { Node2("<", $1, $3) } (* done *)
+ | rawvalue LESSER_OR_EQUAL rawvalue { Node2("<=", $1, $3) } (* done *)
  | TRUE { Leaf (1) }
  | FALSE { Leaf (0) }
 
