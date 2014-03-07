032ede9a9816e05493e7e4d7dc7d1dd48a839373
tungcs <hatungnguyen91@gmail.com>
2014-03-07 20:43:29 +0000

    Create Handle_Nodes_Checklist.ml

---
+++ My_Interpreter/Handle_Nodes_Checklist.ml
@@ -0,0 +1,78 @@
+main:
+  BEGIN LCURLYB body RCURLYB { $3 }
+  | globalVars BEGIN LCURLYB body RCURLYB { Node2("MainWithGlobalVars", $1, $4)}
+;
+
+globalVars:
+ | STRING ASSIGN rawvalue SEMI_COLON { Node2("globalAssign", Variable($1), $3)}
+ | STRING ASSIGN rawvalue SEMI_COLON globalVars { Node2("globalAssignExtending", Node2("globalAssign", Variable($1), $3), $5)} /*Check main to see if handled properly*/
+;
+  
+body:
+     sentence { Node1("bodyEnding", $1) }
+     | sentence body { Node2("bodyExtend", $1, $2) }
+;
+
+/* binding: variable and its value */
+
+variable:
+   | STRING { Variable($1)}
+   | OPENSTREAM rawvalue CLOSESTREAM { Node1("streamValue", $2) }
+
+/* if ( value ) { body }
+   if ( value ) { body } else { body }
+   if ( value ) { body } else_if */
+   
+cond_statement:
+   IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2("if", $3, $6) }
+   | IF LPAREN rawvalue RPAREN LCURLYB body RCURLYB ELSE LCURLYB body RCURLYB { Node3("if", $3, $6, $10) }
+   /* May need to add elseif */
+
+                    
+
+/* while ( value ) { body } */
+while_statement: /* loop */
+   | WHILE LPAREN rawvalue RPAREN LCURLYB body RCURLYB { Node2 ("while", $3, $6) }
+;
+   
+   
+sentence: /* is a statement form a complete instruction, could include a semicolon (?) */
+   | rawvalue SEMI_COLON { $1 }
+   | variable ASSIGN rawvalue SEMI_COLON { Node2("assign", $1, $3) }
+   | variable INCRE_EQUAL rawvalue SEMI_COLON { Node2("+=", $1, $3) }
+   | variable DECRE_EQUAL rawvalue SEMI_COLON { Node2("-=", $1, $3) } /* may need to add *= and /= */
+   | cond_statement { $1 }
+   | while_statement { $1 }
+;
+   
+   
+/* like expression, evaluate to a rawvalue, no semi-colon */
+/*Include: arithmetic operations
+           comparision operations: something that could be avaluated to 1 or 0 (boolean) */
+rawvalue:
+   INT { Leaf($1) }
+ | LPAREN rawvalue RPAREN { $2 }
+ 
+ | variable INCRE /* ++ */ { Node1("++", $1) }
+ | variable DECRE /* -- */ { Node1("--", $1) }
+ 
+ | rawvalue PLUS rawvalue { Node2("+", $1, $3) }
+ | rawvalue MINUS rawvalue { Node2("-", $1, $3) }
+ | rawvalue TIMES rawvalue { Node2("*", $1, $3) }
+ | rawvalue DIV rawvalue { Node2("/", $1, $3) }
+ | rawvalue MOD rawvalue { Node2("%", $1, $3) }
+ | rawvalue EXP rawvalue { Node2("^", $1, $3) }
+ /*| MINUS rawvalue %prec UMINUS { - $2 } */
+  
+ | rawvalue AND rawvalue { Node2("&&", $1, $3) }
+ | rawvalue OR rawvalue { Node2("||", $1, $3) }
+ | NOT rawvalue { Node1("!", $2 )}
+ | rawvalue EQUAL rawvalue { Node2("=", $1, $3) }
+ | rawvalue NOT_EQUAL rawvalue { Node2("!=", $1, $3) }
+ | rawvalue GREATER rawvalue { Node2(">", $1, $3) }
+ | rawvalue GREATER_OR_EQUAL rawvalue { Node2(">=", $1, $3) }
+ | rawvalue LESSER rawvalue { Node2("<", $1, $3) }
+ | rawvalue LESSER_OR_EQUAL rawvalue { Node2("<=", $1, $3) }
+ | TRUE { Leaf (1) }
+ | FALSE { Leaf (0) }
+
