d4ac06e3b0a5be7332179dd66b54ddaea164d357
tungcs <hatungnguyen91@gmail.com>
2014-03-23 18:40:17 +0000

    Update parseTree.ml

--- My_Interpreter/makeInterpreter/parseTree.ml
+++ My_Interpreter/makeInterpreter/parseTree.ml
@@ -1,7 +1,8 @@
 type parseTree = 
- | Leaf of (int) 
- | Variable of (string)
- | Node1 of (string * parseTree)
- | Node2 of (string * parseTree * parseTree)
- | Node3 of (string * parseTree * parseTree * parseTree)
+  | Leaf of (int)
+  | Variable of (string)
+  | Node1 of (string * parseTree)
+  | Node2 of (string * parseTree * parseTree) 
+  | Node3 of (string * parseTree * parseTree * parseTree) 
 ;;
+
